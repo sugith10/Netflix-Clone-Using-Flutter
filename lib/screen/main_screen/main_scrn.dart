f56e50f51245efeb40c7670998e7299bed0b1d56
Sugith <sugithkodappurath@gmail.com>
2024-01-18 19:47:44 +0530

    check

--- lib/screen/main_screen/main_scrn.dart
+++ lib/screen/main_screen/main_scrn.dart
@@ -16,7 +16,7 @@ class _MainScreenState extends State<MainScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-     ApiCall().getRecommedMovies();
+    //  ApiCall().getRecommedMovies();
   }
 
 
@@ -24,34 +24,13 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(115, 0, 0, 0),
-        automaticallyImplyLeading: false,
-        title: WidgetList(context).appbarTitle[_currentIndex],
-        actions: WidgetList(context).appbarActions[_currentIndex],
-      ),
-      body: PopScope(
-        canPop: false,
-        child: WidgetList(context).mainScreens[_currentIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.play_lesson_outlined),
-            label: 'New & Hot',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'My Netflix')
-        ],
-        currentIndex: _currentIndex,
-        onTap: (value) {
-          setState(() {
-            _currentIndex = value;
-          });
-        },
+      body: 
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            
+          ],
+        ),
       ),
     );
   }
